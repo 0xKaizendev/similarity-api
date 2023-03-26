0c17e169ae0d49188ae72be9fd1ab578bbf22682
heyolaniran <olaniranlaurent@gmail.com>
2023-03-26 13:49:17 +0100

    feat: theme toogle components

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,6 +1,10 @@
+'use client'
 import { useTheme } from 'next-themes';
 import {FC} from 'react';
-
+import { DropdownMenu } from './DropdownMenu';
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import Button from './Button';
+import { Sun, Moon, Laptop } from 'lucide-react';
 interface ThemeToggleProps  {
   
 };
@@ -8,9 +12,42 @@ interface ThemeToggleProps  {
 const ThemeToggle:FC<ThemeToggleProps> = ({}) => {
     const {setTheme} = useTheme()
   return (
-    <div>
-      ThemeToggleProps
-    </div>
+    <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+           <Button variant='ghost' size='sm'>
+                <Sun className='rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100'></Sun>
+                <Moon className='absolute rotate-90 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100'></Moon>
+
+                <span className='sr-only'>Toogle theme </span>
+           </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end' forceMount>
+          <DropdownMenuItem onClick={() => {
+            setTheme('light')
+
+          }}>
+              <Sun className='mr-2 h-4 w-4 '></Sun>
+              <span>Light Theme </span>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem onClick={() => {
+            setTheme('dark')
+          }}>
+             <Moon className='mr-2 h-4 w-4'>
+             </Moon>
+             <span>Dark Theme </span>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem onClick={() => {
+            setTheme('system')
+          }}>
+             <Laptop className='mr-2 h-4 w-4'>
+             </Laptop>
+             <span>System Theme </span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+    
   );
 };
 
