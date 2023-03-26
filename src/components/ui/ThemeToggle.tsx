b83b7a3c28c86ebcbadcb1e04aeda70e541a30ec
0xZales <rozalesamiral@gmail.com>
2023-03-26 16:48:31 +0000

    toogle icons

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { useTheme } from 'next-themes';
 import {FC} from 'react';
-import { DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, DropdownMenu  } from './DropdownMenu';
-import Button from './Button';
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, DropdownMenu  } from '@/ui/DropdownMenu';
+import Button from '@/ui/Button';
 import { Sun, Moon, Laptop } from 'lucide-react';
 interface ThemeToggleProps  {
   
@@ -12,39 +12,28 @@ const ThemeToggle:FC<ThemeToggleProps> = ({}) => {
     const {setTheme} = useTheme()
   return (
     <DropdownMenu>
-        <DropdownMenuTrigger asChild >
-           <Button variant='ghost' size='sm'>
-                <Sun className=' rotate-0 scale-100  transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100'></Sun>
-                <Moon className='absolute rotate-90  transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100'></Moon>
-                <span className='sr-only'>Toogle theme </span>
-           </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align='end' forceMount>
-          <DropdownMenuItem onClick={() => {
-            setTheme('light')
-
-          }}>
-              <Sun className='mr-2 h-4 w-4 '></Sun>
-              <span>Light Theme </span>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem onClick={() => {
-            setTheme('dark')
-          }}>
-             <Moon className='mr-2 h-4 w-4'>
-             </Moon>
-             <span>Dark Theme </span>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem onClick={() => {
-            setTheme('system')
-          }}>
-             <Laptop className='mr-2 h-4 w-4'>
-             </Laptop>
-             <span>System Theme </span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-    </DropdownMenu>
+    <DropdownMenuTrigger asChild>
+      <Button variant='ghost' size='sm'>
+        <Sun className='rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100' />
+        <Moon className='absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100' />
+        <span className='sr-only'>Toggle theme</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align='end' forceMount>
+      <DropdownMenuItem onClick={() => setTheme('light')}>
+        <Sun className='mr-2 h-4 w-4' />
+        <span>Light</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem onClick={() => setTheme('dark')}>
+        <Moon className='mr-2 h-4 w-4' />
+        <span>Dark</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem onClick={() => setTheme('system')}>
+        <Laptop className='mr-2 h-4 w-4' />
+        <span>System</span>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
     
   );
 };
