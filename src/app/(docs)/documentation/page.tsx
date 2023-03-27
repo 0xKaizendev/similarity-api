981fefc397fd6933759e1494fe05053439e6f399
heyolaniran <olaniranlaurent@gmail.com>
2023-03-27 12:07:46 +0100

    documentation tabs starting

--- src/app/(docs)/documentation/page.tsx
+++ src/app/(docs)/documentation/page.tsx
@@ -2,6 +2,8 @@ import { FC } from "react"
 
 import type { Metadata } from "next";
 import Heading from '@/ui/Heading'
+import Paragraph from "@/components/ui/Paragraph";
+import DocumentationTabs from "@/components/DocumentationTabs";
 
 export const metadata : Metadata = {
     title :  'Similarity-API | Documentation ', 
@@ -11,10 +13,16 @@ export const metadata : Metadata = {
  
 const page : FC = () => {
     return ( 
-        <div className="container max-w-7xl mx-auto mt-12 ">
-               <div className="flex fex-col items-center gap-6 mt-12">
+        <div className="container  max-w-7xl mx-auto mt-12 ">
+               <div className="flex  justify-center fex-col items-center gap-6 mt-12">
                     <Heading> Documentation Page</Heading>
                 </div> 
+                <div className="mt-4 justify-center flex">
+                 <Paragraph> Our API integration right here to check plagarism percent between your texts </Paragraph>
+                </div>
+
+                <DocumentationTabs/>
+             
         </div>
      );
 }
---
+++ src/components/DocumentationTabs.tsx
@@ -0,0 +1,14 @@
+
+import { FC } from "react";
+
+interface DocumentationProps {
+    
+}
+ 
+const DocumentationTabs: FC<DocumentationProps> = () => {
+    return ( 
+        <div>Documentation Table </div>
+     );
+}
+ 
+export default DocumentationTabs;
