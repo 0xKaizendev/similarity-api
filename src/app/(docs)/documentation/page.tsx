a616b3027db8fd394c56f1911173ae3190663d2b
heyolaniran <olaniranlaurent@gmail.com>
2023-03-27 11:40:29 +0100

    documentation starting

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react"
+
+import type { Metadata } from "next";
+
+
+export const metadata : Metadata = {
+    title :  'Similarity-API | Documentation '
+}
+
+ 
+const page : FC = () => {
+    return ( 
+        <div>
+                Documentation Page
+        </div>
+     );
+}
+ 
+export default page 
