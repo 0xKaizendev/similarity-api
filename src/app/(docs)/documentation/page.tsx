7ea1cb54556a27374adb54e281aae8723626ea2f
heyolaniran <olaniranlaurent@gmail.com>
2023-03-27 11:50:48 +0100

    documentation layout

--- src/app/(docs)/documentation/page.tsx
+++ src/app/(docs)/documentation/page.tsx
@@ -1,17 +1,20 @@
 import { FC } from "react"
 
 import type { Metadata } from "next";
-
+import Heading from '@/ui/Heading'
 
 export const metadata : Metadata = {
-    title :  'Similarity-API | Documentation '
+    title :  'Similarity-API | Documentation ', 
+    description: 'Similarity API'
 }
 
  
 const page : FC = () => {
     return ( 
-        <div>
-                Documentation Page
+        <div className="container max-w-7xl mx-auto mt-12 ">
+               <div className="flex fex-col items-center gap-6 mt-12">
+                    <Heading> Documentation Page</Heading>
+                </div> 
         </div>
      );
 }
