8f56f4d51ef404e34d58f17fd5fde34c339d0076
heyolaniran <olaniran.abd@gmail.com>
2023-04-04 13:04:10 +0100

    revoking api key handler completed

--- src/pages/api/api-key/revoke.ts
+++ src/pages/api/api-key/revoke.ts
@@ -3,6 +3,8 @@ import { authOptions } from "@/libs/auth";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 import { RevokeApiData } from "@/types/api/key";
+import { db } from "@/libs/db";
+import { z } from "zod";
 
 const handler = async (req:  NextApiRequest, res: NextApiResponse <RevokeApiData> ) => {
 
@@ -16,8 +18,36 @@ const handler = async (req:  NextApiRequest, res: NextApiResponse <RevokeApiData
                 success : false 
             })
         }
+
+        const validApiKey  = await db.apiKey.findFirst({
+            where : {userId : user.id , enabled : true}
+        })
+
+        if(!validApiKey) {
+            return res.status(500).json({
+                error : "This API Key could not be revoked", 
+                success : false
+            })
+        }
+
+        // invalidate API  KEY 
+
+        await db.apiKey.update({
+            where : {id : validApiKey.id} , 
+            data : {
+                enabled : false 
+            }
+        })
+
+        return res.status(200).json({error: null , success : true})
     }catch (error ) {
 
+        if(error instanceof z.ZodError) {
+            return res.status(400).json({
+                error: error.issues , 
+                success : false 
+            })
+        }
     }
 
 
