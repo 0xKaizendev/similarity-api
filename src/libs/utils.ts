6db8ba63dfc094c29b54fa7f65202ba661b8a352
0xZales <rozalesamiral@gmail.com>
2023-06-28 22:29:59 +0000

    fix create api

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -6,14 +6,10 @@ export const classNameOptimization = (...inputs: ClassValue[]) => {
 };
 export const createKey = async () => {
   const response = await fetch("/api/api-key/create");
-  console.log("Response",response.json())
-  const data = (await response.json()) as CreateApiData;
-  if (data.error || !data.createdApiKey) {
-    console.log(data.error)
-    if (data.error instanceof Array) {
-      throw new Error(data.error.join(""));
-    }
-    throw new Error(data.error ?? "Something went wrong");
+  if (response.ok) {
+    const data = (await response.json()) as CreateApiData;
+    return data.createdApiKey;
+  } else {
+    throw new Error("Something went wrong");
   }
-  return data.createdApiKey;
 };
