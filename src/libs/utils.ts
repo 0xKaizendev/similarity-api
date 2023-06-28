15476b79190e1d2ea726ea9a67a12a8c82cce1eb
0xZales <rozalesamiral@gmail.com>
2023-06-28 21:27:06 +0000

    display error in prod

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -6,8 +6,10 @@ export const classNameOptimization = (...inputs: ClassValue[]) => {
 };
 export const createKey = async () => {
   const response = await fetch("/api/api-key/create");
+  console.log("Response",response.json())
   const data = (await response.json()) as CreateApiData;
   if (data.error || !data.createdApiKey) {
+    console.log(data.error)
     if (data.error instanceof Array) {
       throw new Error(data.error.join(""));
     }
